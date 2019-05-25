c85eb3c6f7484c0cc9411272c5176ecf1d988559
dvelickovic <djvelickovic15@raf.rs>
2019-05-25 23:06:45 +0200

    Node configuration.

---
+++ node/src/main/java/com/crx/kids/project/node/NetworkEndpoint.java
@@ -0,0 +1,26 @@
+package com.crx.kids.project.node;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+
+@RestController
+@RequestMapping(path = "net")
+public class NetworkEndpoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(NetworkEndpoint.class);
+
+    @GetMapping(path = "neighbours", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity getAllNeighbours() {
+        logger.info(System.getProperty("bootstrap.addr"));
+        return ResponseEntity.ok().build();
+    }
+
+
+}
---
+++ node/src/main/java/com/crx/kids/project/node/NodeApplication.java
@@ -0,0 +1,15 @@
+package com.crx.kids.project.node;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodeApplication {
+
+    // bootstrap port
+    // bootsrap address
+    public static void main(String[] args) {
+
+        SpringApplication.run(NodeApplication.class, args);
+    }
+}
